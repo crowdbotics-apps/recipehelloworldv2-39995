96298e1c0c73de1812f706c8dd4f0cde2a434017
Crowdbotics <team@crowdbotics.com>
2023-04-18 01:19:01 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/recipehelloworldv2_39995/MainActivity.java
@@ -0,0 +1,15 @@
+package com.recipehelloworldv2_39995;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "recipehelloworldv2_39995";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "recipehelloworldv2-39995",
+  url: "https://recipehelloworldv2-39995.botics.co",
+  api: "https://recipehelloworldv2-39995.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
